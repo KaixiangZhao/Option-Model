9487567b4bf0b8e138bda148e20619390d10961c
MichaelZhao <michaelzhao826@gmail.com>
2015-03-30 00:02:20 +0800

    Add function to plot price-sigma curve

--- plot.py
+++ plot.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
-from calculate import WARRANT_LIST, calculate, input_all
+import numpy
+import math
+import scipy.stats
+from calculate import WARRANT_LIST, calculate, input_all, stock_sort, R
 
 def plot(warrant, bs_result, nw_result, bsda_result, ukhov_result):
     bs_plot = []
@@ -63,6 +66,33 @@ def get_plot(code):
              bsda_result, ukhov_result) = calculate(warrant)
             plot(warrant, bs_result, nw_result, bsda_result, ukhov_result)
 
+def plot_sigma(warrant):
+    sigma_list = numpy.linspace(0, 0.5, num=101)
+    result_list = []
+    stock_match = []
+    for day in warrant.target_stock.everyday_price:
+        if (day["Date"] - warrant.end_date).days < 0 and \
+           (warrant.start_date - day["Date"]).days <= 365:
+            stock_match.append(day)
+
+    stock_sort(stock_match)
+
+    for sigma in sigma_list:
+        t = (warrant.end_date - stock_match[1]["Date"]).days
+        d_1 = (numpy.log(stock_match[1]["price"] / warrant.price) + \
+               (R + 0.5 * sigma * sigma) * t) / (sigma * math.pow(t, 0.5))
+        d_2 = d_1 - sigma * math.pow(t, 0.5)
+        result = stock_match[1]["price"] * scipy.stats.norm.cdf(d_1) \
+                     - warrant.price * math.exp(-R * t) * \
+                     scipy.stats.norm.cdf(d_2)
+        result_list.append(result)
+
+    print(stock_match[1]["price"])
+    plt.plot(sigma_list, result_list)
+    plt.show()
+
+
 if __name__ == '__main__':
     input_all()
-    get_plot('031001')
+    #get_plot('031001')
+    plot_sigma(WARRANT_LIST[0])
