9ce87ac0c097da980f7704b96c8e0f141a2dbf9f
MichaelZhao <michaelzhao826@gmail.com>
2015-04-04 10:27:10 +0800

    Some small changes

--- plot.py
+++ plot.py
@@ -67,32 +67,33 @@ def get_plot(code):
             plot(warrant, bs_result, nw_result, bsda_result, ukhov_result)
 
 def plot_sigma(warrant):
-    sigma_list = numpy.linspace(0, 0.5, num=101)
+    sigma_list = numpy.linspace(0, 1, num=101)
     result_list = []
     stock_match = []
     for day in warrant.target_stock.everyday_price:
         if (day["Date"] - warrant.end_date).days < 0 and \
-           (warrant.start_date - day["Date"]).days <= 365:
+           (warrant.start_date - day["Date"]).days <= 0:
             stock_match.append(day)
 
     stock_sort(stock_match)
 
     for sigma in sigma_list:
-        t = (warrant.end_date - stock_match[300]["Date"]).days
-        d_1 = (numpy.log(stock_match[300]["price"] / warrant.price) + \
+        t = (warrant.end_date - stock_match[2]["Date"]).days
+        d_1 = (numpy.log(stock_match[2]["price"] / warrant.price) + \
                (R + 0.5 * sigma * sigma) * t) / (sigma * math.pow(t, 0.5))
         d_2 = d_1 - sigma * math.pow(t, 0.5)
-        result = stock_match[300]["price"] * scipy.stats.norm.cdf(d_1) \
+        result = stock_match[2]["price"] * scipy.stats.norm.cdf(d_1) \
                      - warrant.price * math.exp(-R * t) * \
                      scipy.stats.norm.cdf(d_2)
         result_list.append(result)
 
-    print(stock_match[1]["price"])
+    print(stock_match[2]["price"])
+    print(result_list)
     plt.plot(sigma_list, result_list)
     plt.show()
 
 
 if __name__ == '__main__':
     input_all()
-    get_plot('031005')
-    #plot_sigma(WARRANT_LIST[0])
+    #get_plot('031005')
+    plot_sigma(WARRANT_LIST[0])
